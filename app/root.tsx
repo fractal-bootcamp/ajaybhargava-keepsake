2b071d292f9702122d0df7a7ec45fdffc4f9aa74
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-14 15:41:59 -0500

    fallback URLS

--- app/root.tsx
+++ app/root.tsx
@@ -61,8 +61,23 @@ export default function App({ loaderData }: Route.ComponentProps) {
 			signInFallbackRedirectUrl="/"
 		>
 			<header className="flex items-center justify-center py-8 px-4" />
-			<main className="max-w-screen-md mx-auto">
-				<Outlet />
+			<main className="relative border p-8 max-w-screen-md w-full mx-auto">
+				<div className="absolute top-4 right-4">
+					<SignedOut>
+						<SignInButton />
+					</SignedOut>
+					<SignedIn>
+						<UserButton />
+					</SignedIn>
+				</div>
+				<div className="flex flex-col items-center gap-4">
+					<h1 className="text-4xl font-bold">Keepsake</h1>
+					<p className="text-lg">
+						If you want to store a memory when you're logged out, you're NGMI.
+						🫵😂
+					</p>
+					<Outlet />
+				</div>
 			</main>
 		</ClerkProvider>
 	);
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,23 +6,5 @@ import {
 } from "@clerk/react-router";
 
 export function Welcome() {
-	return (
-		<main className="relative border p-8 max-w-screen-md w-full mx-auto">
-			<div className="absolute top-4 right-4">
-				<SignedOut>
-					<SignInButton />
-				</SignedOut>
-				<SignedIn>
-					<UserButton />
-				</SignedIn>
-			</div>
-			<div className="flex flex-col items-center gap-4">
-				<h1 className="text-4xl font-bold">Keepsake</h1>
-				<p className="text-lg">
-					If you want to store a memory when you're logged out, you're NGMI.
-					🫵😂
-				</p>
-			</div>
-		</main>
-	);
+	return <main className="">Hey.</main>;
 }
